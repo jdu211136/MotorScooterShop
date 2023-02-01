749f2a55777ebc1853ca66fd2f9c4af3da7cbf5f
Mukhammed <mukhamm0@gmail.com>
2023-02-01 10:36:49 +0500

    copied

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-import products from "./products.json" assert(type: "json")
+import products from "./products.json" assert { type: "json" };
+const content = document.querySelector("#content");
+
+for (let i = 0; i < products.length; i++) {
+  const product = document.createElement("div");
+  product.innerHTML = `
+    <img src="images/${products[i].image}"><br>
+    Name: ${products[i].name}<br>
+    Price: ${products[i].currency}${products[i].price}<br>
+    `;
+  content.append(product);
+}
